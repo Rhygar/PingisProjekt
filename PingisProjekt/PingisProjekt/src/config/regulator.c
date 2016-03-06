bcc2f705fefda0793e32a32353ad7f3c4e1b867e
Rhygar <john_tengvall@hotmail.com>
2016-03-06 16:49:19 +0100

    The regulator task

--- PingisProjekt/PingisProjekt/src/config/regulator.c
+++ PingisProjekt/PingisProjekt/src/config/regulator.c
@@ -2,5 +2,15 @@
  * regulator.c
  *
  * Created: 2016-03-06 16:29:32
- *  Author: Spellabbet
+ *  Author: s
  */ 
+#include <asf.h>
+#include "semafor.h"
+#include "adc.h"
+#include <FreeRTOS.h>
+#include "pwm.h"
+
+void task_regulator(void *pvParameters)
+{
+	portTickType xLastWakeTime; 
+}
